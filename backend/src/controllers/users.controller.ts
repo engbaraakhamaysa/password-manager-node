a7a34ab0790aa6332ec4288e7adfb65de5dbc821
Baraa <engbaraakhamaysa@gmail.com>
2026-07-01 05:11:55 +0300

    feat: Simple auth & middleware

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,13 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../models/User";
 
-//create user endpoint
-export const createUser = async (req: Request, res: Response) => {
-  const user = await User.create(req.body);
-
-  res.json(user);
-};
-
 //get all users endpoint
 export const getUsers = async (req: Request, res: Response) => {
   const users = await User.find();
@@ -20,4 +13,23 @@ export const getUserById = async (req: Request, res: Response) => {
   const user = await User.findById(req.params.id);
 
   res.json(user);
+};
+
+//update users
+export const updateUser = async (req: Request, res: Response) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.json(user);
+};
+
+//Delete User
+
+export const deleteUser = async (req: Request, res: Response) => {
+  await User.findByIdAndDelete(req.params.id);
+
+  res.json({
+    message: "User delete",
+  });
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
-  createUser,
   getUsers,
   getUserById,
+  updateUser,
+  deleteUser,
 } from "../controllers/users.controller";
+
+import { createUser } from "../controllers/auth.controller";
+
+import { protect } from "../middleware/authMiddleware";
 
 const router = Router();
 
@@ -12,6 +17,10 @@ router.post("/", createUser);
 router.get("/", getUsers);
 
 router.get("/:id", getUserById);
+
+router.put("/:id", updateUser);
+
+router.delete("/:id", protect, deleteUser);
 
 router.get("/", (req, res) => {
   res.json("User API");
