fb9bdefab3bde07ad10211d7c2678adf46e35f44
Baraa <engbaraakhamaysa@gmail.com>
2026-07-05 05:26:49 +0300

    refactor: remove obsolete JavaScript source files

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,23 +1,27 @@
 import mongoose, { Schema } from "mongoose";
+import { IUser } from "../types/user.types";
 
-const userSchema = new Schema(
+const userSchema = new Schema<IUser>(
   {
     name: {
       type: String,
-      required: true,
-      minlength: 3,
+      required: [true, "Name is required"],
+      minlength: [3, "Name must be at least 3 characters"],
+      trim: true,
     },
 
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
+      lowercase: true,
+      trim: true,
     },
 
     password: {
       type: String,
-      reqired: true,
-      minlength: 6,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters"],
     },
 
     role: {
@@ -31,4 +35,21 @@ const userSchema = new Schema(
   },
 );
 
-export const User = mongoose.model("User", userSchema);
+// userSchema.set("toJSON", {
+//   transform: function (doc, ret) {
+//     delete ret.password;
+//     return ret;
+//   },
+// });
+
+userSchema.set("toJSON", {
+  transform: function (doc, ret) {
+    const obj = ret as Partial<IUser>;
+
+    delete obj.password;
+
+    return obj;
+  },
+});
+
+export const User = mongoose.model<IUser>("User", userSchema);
--- backend/src/routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllUsers } = require("../controllers/usersController");
-//path get all users in wibset to Admin
-router.get("/", getAllUsers);
-
-module.exports = router;
