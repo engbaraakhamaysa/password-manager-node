f96d74fab9852e0a0488287a49887ff49e578fd3
Baraa <engbaraakhamaysa@gmail.com>
2026-07-05 10:27:21 +0300

    feat: implement auth services and update login/signup logic

---
+++ frontend/src/services/authServices.js
@@ -0,0 +1,13 @@
+import api from "./api";
+
+export const authService = {
+  register: async (data) => {
+    const res = await api.post("/auth/register", data);
+    return res.data;
+  },
+
+  login: async (data) => {
+    const res = await api.post("/auth/login", data);
+    return res.data;
+  },
+};
